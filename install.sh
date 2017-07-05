b10a53e9e6d8aa163ad54a7674499b82a539f115
Emiliano Zublena <ezublena@gmail.com>
2017-07-04 23:23:06 -0300

    agregados pasos de laravel

--- install.sh
+++ install.sh
@@ -11,6 +11,20 @@ echo "Ejecutando composer update..."
 composer update
 sudo chgrp -R www-data storage bootstrap/cache
 sudo chmod -R ug+rwx storage bootstrap/cache
+mv .env.example .env
+php artisan key:generate
+echo "Copia la clave generada e ingresala"
+read app_key
+sed -i -- 's/{app_key}/$app_key/g' .env
+echo "Ingresa la base de datos"
+read database_name
+sed -i -- 's/{database_name}/$database_name/g' .env
+echo "Ingresa el usuario de la base de datos"
+read database_username
+sed -i -- 's/{database_username}/$database_username/g' .env
+echo "Ingresa la contraseña de la base de datos"
+read database_password
+sed -i -- 's/{database_password}/$database_password/g' .env
 echo "Querés ejecutar las migraciones de laravel? (si/no)"
 read laravel_migrations
 if [ "$laravel_migrations" = "si" ]
@@ -31,5 +45,11 @@ if [ "$passport_bool" = "si" ]
 then
 	echo "Creando clients de passport"
 	php artisan passport:install
+	echo "Ingresa la secret key del client 1"
+	read personal_secret
+	sed -i -- 's/{personal_secret}/$personal_secret/g' .env
+	echo "Ingresa la secret key del client 2"
+	read password_secret
+	sed -i -- 's/{password_secret}/$password_secret/g' .env
 fi
 echo "Instalado, recorda ejecutar virtual_hosts.sh"
