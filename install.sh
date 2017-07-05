156a5ee1de6d87a4f6b9e02635c7ee55259804e0
Emiliano Zublena <ezublena@gmail.com>
2017-07-04 23:29:44 -0300

    s

--- install.sh
+++ install.sh
@@ -15,16 +15,16 @@ mv .env.example .env
 php artisan key:generate
 echo "Copia la clave generada e ingresala"
 read app_key
-sed -i -- 's/{app_key}/$app_key/g' .env
+sed -i -- "s/{app_key}/$app_key/g" .env
 echo "Ingresa la base de datos"
 read database_name
-sed -i -- 's/{database_name}/$database_name/g' .env
+sed -i -- "s/{database_name}/$database_name/g" .env
 echo "Ingresa el usuario de la base de datos"
 read database_username
-sed -i -- 's/{database_username}/$database_username/g' .env
+sed -i -- "s/{database_username}/$database_username/g" .env
 echo "Ingresa la contraseña de la base de datos"
 read database_password
-sed -i -- 's/{database_password}/$database_password/g' .env
+sed -i -- "s/{database_password}/$database_password/g" .env
 echo "Querés ejecutar las migraciones de laravel? (si/no)"
 read laravel_migrations
 if [ "$laravel_migrations" = "si" ]
@@ -47,9 +47,9 @@ then
 	php artisan passport:install
 	echo "Ingresa la secret key del client 1"
 	read personal_secret
-	sed -i -- 's/{personal_secret}/$personal_secret/g' .env
+	sed -i -- "s/{personal_secret}/$personal_secret/g" .env
 	echo "Ingresa la secret key del client 2"
 	read password_secret
-	sed -i -- 's/{password_secret}/$password_secret/g' .env
+	sed -i -- "s/{password_secret}/$password_secret/g" .env
 fi
 echo "Instalado, recorda ejecutar virtual_hosts.sh"
