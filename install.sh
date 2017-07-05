d6b5b3ccc938cb462e19a6d985593ae85655f772
Emiliano Zublena <ezublena@gmail.com>
2017-07-04 23:14:12 -0300

    agregados pasos de laravel

--- install.sh
+++ install.sh
@@ -13,21 +13,21 @@ sudo chgrp -R www-data storage bootstrap/cache
 sudo chmod -R ug+rwx storage bootstrap/cache
 echo "Querés ejecutar las migraciones de laravel? (si/no)"
 read laravel_migrations
-if[ "$laravel_migrations" = "si" ]
+if [ "$laravel_migrations" = "si" ]
 then
 	echo "Ejecutando migraciones de laravel..."
 	php artisan migrate:refresh
 fi
 echo "Querés ejecutar las seeds de la db? (si/no)"
 read database_seeds
-if[ "$database_seeds" = "si" ]
+if [ "$database_seeds" = "si" ]
 then
 	echo "Seedeando la base..."
 	php artisan db:seed
 fi
 echo "Utilizas Passport? (si/no)"
 read passport_bool
-if[ "$passport_bool" = "si" ]
+if [ "$passport_bool" = "si" ]
 then
 	echo "Creando clients de passport"
 	php artisan passport:install
